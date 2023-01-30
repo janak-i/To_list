1738c377d108191aebc4961bd1301b38d1465d1b
Janaklodhi <janakthakur346@gmail.com>
2023-01-30 20:05:58 +0530

    changes in code

--- db/migrate/20230130071402_create_users.rb
+++ db/migrate/20230130071402_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :email
       t.string :password
 
+
       t.timestamps
     end
   end
--- db/migrate/20230130071432_create_todos.rb
+++ db/migrate/20230130071432_create_todos.rb
@@ -2,6 +2,7 @@ class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
       t.string :title
+      t.references :user
 
       t.timestamps
     end
